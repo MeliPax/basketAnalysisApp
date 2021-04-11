5bb012b0af3c8888628df6cc02f30bdc5837c8b1
MeliPax <33260323+MeliPax@users.noreply.github.com>
2021-04-11 18:46:03 +0200

    Updated ui and user input style

--- web.py
+++ web.py
@@ -1,94 +1,90 @@
-#  Importing libraries
-import streamlit as st
+#  Importing libraries 
 import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
-import matplotlib.pyplot as plt
-from matplotlib import style
 import numpy as np
+import streamlit as st
 
-# the Main function
-html_temp = """
-<div style="background-color:#9932CC;padding:10px">
-<h2 style="color:white; text-align:center;">Basket Analysis App</h2>
-</div>
-"""
 
-st.markdown(html_temp, unsafe_allow_html=True)
-product = st.text_input("Enter a product to generate basket", "")
-
-#  Importing the dataset. This data set is for an online retail shop, containing more than 100 thousand sales records for 1 year (2018-2019)
-datadf = pd.read_csv("transaction_data.csv")
+#  Importing the dataset. This data set is for an online retail shop, containing more than 100 thousand sales records for 1 year (2018-2019)   
+datadf = pd.read_csv("drive/My Drive/datasets/transaction_data.csv", encoding='latin1')
 
 df = datadf.copy()
 
-
 def clean_data(df):
-    #  Cleaning data
-    df = df[df.UserId > 0]
-    df = df[df.ItemCode > 0]
-    df = df[df.NumberOfItemsPurchased > 0]
-    df = df[df.CostPerItem > 0]
-    df = df[df.ItemDescription.notna()]
-    df = df[df.TransactionTime.str[-4:] != '2028']
-    return df
-
+  #  Cleaning data 
+  df = df[df.UserId>0] 
+  df = df[df.ItemCode>0]
+  df = df[df.NumberOfItemsPurchased>0]
+  df = df[df.CostPerItem>0]
+  df = df[df.ItemDescription.notna()]
+  df = df[df.TransactionTime.str[-4:] != '2028']
+  return df
 
 def encode(x):
-    if x <= 0:
-        return 0
-    else:
-        return 1
-
+ if x <= 0:
+  return 0
+ else:
+  return 1
 
 def toString(x):
-    string = '' + str(x)
-    string = string[12:-3:1]
-    return string
-
+  string = '' + str(x)
+  string=string[12:-3:1]
+  return string
 
 def product_exist(prod):
-    return prod in rules_list
-
+  return prod in rules_list
 
 def find_product_rules(product):
-    if (product_exist(product) == True):
-        results = rules[rules['antecedents'] == product]
-        return results
-    else:
-        message = "Poduct has no basket yet"
-        return message
+  if (product_exist(product) == True):
+    results = rules[rules['antecedents']== product]
+    return results
+  else:
+    message ="Poduct has no basket yet"
+    return message
 
 
 df = clean_data(df)
 
+# retrieving item names in the dataset
+prod_des=df['ItemDescription'].unique()
+item_list=['']
+
+for x in prod_des:
+  item_list.append(x)
+
 #  Re-arranged the dataframe into a matrix of products per transaction
-df_set = df.groupby(['TransactionId', 'ItemDescription']).NumberOfItemsPurchased.sum(
-).unstack().reset_index().fillna(0).set_index('TransactionId')
+df_set = df.groupby(['TransactionId', 'ItemDescription']).NumberOfItemsPurchased.sum().unstack().reset_index().fillna(0).set_index('TransactionId')
 
 df_set = df_set.applymap(encode)
 
 # Applied Apriori algorithm to get the frequently bought item-sets
-frequent_itemsets = apriori(df_set, min_support=0.015, use_colnames=True)
+frequent_itemsets = apriori(df_set, min_support = 0.015, use_colnames = True)
 
-# Applied the association rules on the item-sets
+# Applied the association rules on the item-sets 
 # Tuned the confidence minimum threshold to 0.2
-rules = association_rules(
-    frequent_itemsets, metric='confidence', min_threshold=0.20)
+rules = association_rules(frequent_itemsets, metric = 'confidence', min_threshold = 0.20)
 
-rules['antecedents'] = rules['antecedents'].apply(lambda x: toString(x))
-rules['consequents'] = rules['consequents'].apply(lambda x: toString(x))
+rules['antecedents']=rules['antecedents'].apply(lambda x: toString(x))
+rules['consequents']=rules['consequents'].apply(lambda x: toString(x))
 
-rules_list = rules["antecedents"].to_list()
+rules_list=rules["antecedents"].to_list()
 
-st.success('The predicted category of the article is: {}'.format(result))
-html_temp = """
-<div style="background-color:grey;padding:6px">
-<h6 style="color:white;">Other related articles</h3>
-</div>
-"""
+# front end elements of the web page 
 
-st.button("Compute basket")
-if st.button("Compute basket"):
-    st.markdown(html_temp, unsafe_allow_html=True)
-    st.write(find_product_rules(product))
+html_temp = """ 
+<div style ="background-color:#85c1ff;padding:13px"> 
+<h1 style ="color:black;text-align:center;">Basket analysis Web App</h1> 
+</div> 
+""" 
+
+st.markdown(html_temp, unsafe_allow_html = True) 
+input_product = st.selectbox("Search and Select product", item_list)
+
+if (input_product):
+  if (input_product==''):
+    st.write("Wrong input, kindly select a product from the list.")
+  else:
+    results= find_product_rules(input_product)
+    st.write("Basket results")
+    st.write(results)
