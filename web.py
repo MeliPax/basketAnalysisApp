8f02e4fff1a71380e81d6aca85352547590e7d48
MeliPax <33260323+MeliPax@users.noreply.github.com>
2021-04-11 19:07:38 +0200

    Bug fix: Dataset loading

--- web.py
+++ web.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 #  Importing the dataset. This data set is for an online retail shop, containing more than 100 thousand sales records for 1 year (2018-2019)   
-datadf = pd.read_csv("drive/My Drive/datasets/transaction_data.csv", encoding='latin1')
+datadf = pd.read_csv("transaction_data.csv")
 
 df = datadf.copy()
 
